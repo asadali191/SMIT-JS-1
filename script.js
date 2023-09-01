60e3c72ee2549e247b4d7f68628bdde0bf5a9613
asadali191 <139580778+asadali191@users.noreply.github.com>
2023-09-01 15:52:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+//chapter - 01
+                              //Question - 01
+
+var greetingmesssage;
+greetingmesssage="Hello World";
+alert(greetingmesssage);
+                              //Question -02
+var message;
+message="error! please enter a valid password";
+alert(message);
+                                //Question -03
+var message;
+message="Welcome to the Js land...\n happy coding! ";
+alert(message);
+                            //Question -04
+
+alert("Welcome to Js Land");alert("happy coding");
+
+
+                               //Question -05
+var message;
+message="Hello.. i can run js through my web browser console";
+alert(message );
+                        
+                             //Chapter -02
+
+                            //Question -01
+ var username;
+                            //Question -02
+var myname;
+myname="Asad Ali"
+                             //Question -03
+var Message;
+Message="Hello World"
+alert(messsag);
+                            //Question -04
+var student;
+student="Asad Ali";
+var age;
+age="19 year old";
+var prof;
+prof="certified web and app developer";
+alert(student);alert(age);alert(prof);
+                             //Question -05
+var a="PIZZA";
+
+ alert(a+"\n"+a[0]+a[1]+a[2]+a[3]+"\n"+a[0]+a[1]+a[2]+"\n"+a[0]+a[1]+"\n"+a[0]);
+
+                            //Question -06
+var email;
+ email="example@example.com"; 
+ alert("My email address is "+email);
+
+                             //Question -07
+var book;
+book='A smarter way to learn JavaScript';
+alert("I am trying to learnfrom the book \n"+book)
+                              //Question -08
+document.write("Yah I can write HTML content through Javascript");
+                               //Question -09 
+alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
